27866e50c0065859c1ee04c94d30eac60e434a2b
Quentin Gabriele <quentin.gabriele@gmail.com>
2024-09-05 22:38:49 +0200

    add test

---
+++ tests/Unit/KpiMoneyDefinitionTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use Brick\Money\Money;
+use Elegantly\Kpi\KpiMoneyDefinition;
+use Elegantly\Kpi\Models\Kpi;
+
+it('can calculate the difference between two money KPI', function () {
+    $old = new Kpi([
+        'value' => Money::of(100, 'USD'),
+    ]);
+
+    $new = new Kpi([
+        'value' => Money::of(200, 'USD'),
+    ]);
+
+    $diff = KpiMoneyDefinition::diff($old, $new);
+
+    expect(
+        $diff?->getAmount()->toFloat()
+    )->toBe(
+        100.0
+    );
+});
